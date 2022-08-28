0409934d2cb709f39e702a5aa2f51bf8db39621c
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2022-08-28 21:56:47 +0200

    1.2

--- improved_drowning_system/lua/autorun/server/improved_drowning_system.lua
+++ improved_drowning_system/lua/autorun/server/improved_drowning_system.lua
@@ -10,79 +10,70 @@ local improved_drowning_sys_preventleave = 5 --- If the player gets out of the w
 do
     local improved_sound_sys = improved_sound_sys or {}
     local improved_sound_max, improved_caching_id = 2
- 
+    
     local function improved_clear_sound(id)
-       if improved_sound_sys[id] then
-          for i = 1, improved_sound_max do
-             improved_sound_sys[id][i]:Stop()
-          end
- 
-          improved_sound_sys[id] = nil
-       end
+        if improved_sound_sys[id] then
+            for i = 1, improved_sound_max do
+                improved_sound_sys[id][i]:Stop()
+            end
+            improved_sound_sys[id] = nil
+        end
     end
- 
     local function improved_drowning_system()
-       local improved_class_sys = ents.FindByClass("player")
- 
-       for _, v in ipairs (improved_class_sys) do
-          if (v:WaterLevel() >= 3) then
-             if v:Alive() then
-                v.improved_player_water = (v.improved_player_water or 0) + 1
- 
-                if (v.improved_player_water <= improved_drowning_sys_sec) then
-                   v:EmitSound("player/pl_drown" ..math.random(1, 3).. ".wav", 75, 100, 1, CHAN_AUTO)
- 
-                else
-                   improved_caching_id = v:UserID()
- 
-                   if not improved_sound_sys[improved_caching_id] then
-                      improved_sound_sys[improved_caching_id] = {}
- 
-                      for i = 1, improved_sound_max do
-                         improved_sound_sys[improved_caching_id][i] = i == 1 and CreateSound(v, "player/heartbeat1.wav") or i == 2 and CreateSound(v, "player/breathe1.wav")
-                      end
-                   else
-                      if improved_sound_sys[improved_caching_id][2]:IsPlaying() then
-                         improved_sound_sys[improved_caching_id][2]:Stop()
-                      end
-                   end
- 
-                   v:EmitSound("player/pl_drown" ..math.random(1, 3).. ".wav", 35, math.random(5, 20), 1, CHAN_AUTO)
- 
-                   if (v.player_inwater_cur and v.player_inwater_cur > 0) then
-                      v.player_inwater_cur = 0
-                   end
-                   improved_sound_sys[improved_caching_id][1]:PlayEx(1, 100)
-                   v:SetHealth(v:Health() - improved_drowning_sys_healthdamage)
- 
-                   if (v:Health() <= 0) then
-                      v:Kill()
- 
-                      v.improved_player_water  = nil
-                      v.player_inwater_cur = nil
- 
-                      improved_clear_sound(improved_caching_id)
-                   end
+        local improved_class_sys = ents.FindByClass("player")
+
+        for _, v in ipairs (improved_class_sys) do
+            if (v:WaterLevel() >= 3) then
+                if v:Alive() then
+                    v.improved_player_water = (v.improved_player_water or 0) + 1
+                    
+                    if (v.improved_player_water <= improved_drowning_sys_sec) then
+                        v:EmitSound("player/pl_drown" ..math.random(1, 3).. ".wav", 75, 100, 1, CHAN_AUTO)
+                    else
+                        improved_caching_id = v:UserID()
+                        if not improved_sound_sys[improved_caching_id] then
+                            improved_sound_sys[improved_caching_id] = {}
+                            for i = 1, improved_sound_max do
+                                improved_sound_sys[improved_caching_id][i] = i == 1 and CreateSound(v, "player/heartbeat1.wav") or i == 2 and CreateSound(v, "player/breathe1.wav")
+                            end
+                        else
+                            if improved_sound_sys[improved_caching_id][2]:IsPlaying() then
+                                improved_sound_sys[improved_caching_id][2]:Stop()
+                            end
+                        end
+                        v:EmitSound("player/pl_drown" ..math.random(1, 3).. ".wav", 35, math.random(5, 20), 1, CHAN_AUTO)
+
+                        if (v.player_inwater_cur and v.player_inwater_cur > 0) then
+                            v.player_inwater_cur = 0
+                        end 
+                        
+                        improved_sound_sys[improved_caching_id][1]:PlayEx(1, 100)                        
+                        v:SetHealth(v:Health() - improved_drowning_sys_healthdamage)
+                        
+                        if (v:Health() <= 0) then
+                            v:Kill()
+                            v.improved_player_water  = nil
+                            v.player_inwater_cur = nil
+                            improved_clear_sound(improved_caching_id)
+                        end
+                    end
                 end
-             end
-          else
-             if v.improved_player_water then
-                improved_caching_id = v:UserID()
- 
-                if improved_sound_sys[improved_caching_id] then
-                   improved_sound_sys[improved_caching_id][2]:PlayEx(1, 100)
+            else
+                if v.improved_player_water then
+                    improved_caching_id = v:UserID()
+                    if improved_sound_sys[improved_caching_id] then
+                        improved_sound_sys[improved_caching_id][2]:PlayEx(1, 100)
+                    end
+                    v.player_inwater_cur = (v.player_inwater_cur or 0) + 1
+                    
+                    if (v.player_inwater_cur >= improved_drowning_sys_preventleave) then
+                        v.improved_player_water  = nil
+                        v.player_inwater_cur = nil
+                        improved_clear_sound(improved_caching_id)
+                    end
                 end
-                v.player_inwater_cur = (v.player_inwater_cur or 0) + 1
- 
-                if (v.player_inwater_cur >= improved_drowning_sys_preventleave) then
-                   v.improved_player_water  = nil
-                   v.player_inwater_cur = nil
- 
-                   improved_clear_sound(improved_caching_id)
-                end
-             end
-          end
-       end
+            end
+        end
     end
     timer.Create("ImprovedDrowning_Sys", 1, 0, improved_drowning_system)
- end
+end
